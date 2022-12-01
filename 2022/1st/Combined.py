0ec2af36eed16e6bca7ff19e112aa26c5490be3f
Petteri Niemi <beteniemi@gmail.com>
2022-12-01 14:19:28 +0200

    Implement combiined solution for both parts

---
+++ 2022/1st/Combined.py
@@ -0,0 +1,16 @@
+max = 0
+file = open("[YourInputFilePath]\input.txt", "r")
+elfInv = 0
+topThree = [0, 0, 0]
+for row in file:
+    if row != "\n":
+        elfInv += int(row)
+        if elfInv > topThree[0]:
+            topThree[0] = elfInv
+    else:
+        topThree.sort()
+        elfInv = 0
+
+topThree.sort()
+print(topThree[2])
+print(sum(topThree))
