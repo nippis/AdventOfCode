39417ed5e02acf364a3746fc92cd9a7daf19671e
Petteri Niemi <beteniemi@gmail.com>
2022-12-02 17:10:48 +0200

    Implement part 2 with C++ and make code compact

--- 2022/2nd/solution.cpp
+++ 2022/2nd/solution.cpp
@@ -1,29 +1,17 @@
 #include <fstream>
 #include <iostream>
 
-int get_result(int opp, int you)
-{
-    if (opp==you) return 1;
-    else if (you>>2&opp|you<<1&opp) return 0;
-    else if (you<<2&opp|you>>1&opp) return 2;
-    else return -1;
-}
-
-void main()
-{
-    std::string row = "";
+int main() {
+    std::string row;
     std::ifstream file ("input.txt");
-    int points = 0;
-    while (getline(file, row)) 
-    {
-        int opp = 1;
-        int you = 1;
-        if (row.size() > 3)
-        {
-            opp <<= (int)row.at(0) - 65;
-            you <<= (int)row.at(2) - 88;
-            points += get_result(opp, you)*3 + (you>>1)+1;
-        }
+    int a = 0, b = 0;
+    while (getline(file, row)) {
+        int opp = 1<<(int)row.at(0)-65;
+        int you = 1<<(int)row.at(2)-88;
+        a += (opp==you?1:(you>>2&opp|you<<1&opp?0:2))*3 + (you>>1)+1;
+        b += (((you&1)?(opp&1?opp<<2:opp>>1):(you&2?opp:(opp&4?opp>>2:opp<<1)))>>1)+1 + (you>>1)*3;
     }
-    std::cout << points << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    return 0;
 }
