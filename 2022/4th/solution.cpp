4dc7298d782c6f78eaa7239a228378abb474d740
Petteri Niemi <beteniemi@gmail.com>
2022-12-04 11:03:41 +0200

    Implement day 2 tasks

--- 2022/4th/solution.cpp
+++ 2022/4th/solution.cpp
@@ -1,14 +1,37 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+
+int getLimits(std::string row, std::vector<int>& limits)
+{
+  limits.clear();
+  std::vector<std::string> limitStrings = {""};
+  for (char a : row)
+  {
+    if (a == '-' || a == ',') limitStrings.push_back("");
+    else
+    {
+        limitStrings.back() += a;
+    }
+  }
+  for (std::string str : limitStrings)
+    limits.push_back(std::stoi(str));
+  return limits.size();
+}
 
 int main()
 {
-    std::ifstream file("input.txt");
-    std::string row;
-    int a = 0, b = 0;
-    while (getline(file, row))
-    {
-
-    }
-    std::cout << a << std::endl << b << std::endl;
+  std::ifstream file("input.txt");
+  std::string row;
+  std::vector<int> limits;
+  int a = 0, b = 0;
+  while (getline(file, row))
+  {
+    getLimits(row, limits);
+    a += (limits.at(0)>=limits.at(2) && limits.at(1)<=limits.at(3) ||
+          limits.at(0)<=limits.at(2) && limits.at(1)>=limits.at(3));
+    b += (limits.at(1)>=limits.at(2) && limits.at(0)<=limits.at(2) ||
+          limits.at(3)>=limits.at(0) && limits.at(2)<=limits.at(0));
+  }
+  std::cout << a << std::endl << b << std::endl;
 }
