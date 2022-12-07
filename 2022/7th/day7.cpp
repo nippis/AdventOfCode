41e2eb10fc50b45d66ff4cfaba79cddd790c5989
Petteri Niemi <beteniemi@gmail.com>
2022-12-07 12:29:22 +0200

    Allocate memory dynamically

--- 2022/7th/day7.cpp
+++ 2022/7th/day7.cpp
@@ -1,58 +1,49 @@
 #include <memory>
 #include "../solutions.hh"
 
-struct file_tree_node
+typedef struct file_tree_node
 {
-  file_tree_node(const std::string& name) : m_name(name) {};
-  std::shared_ptr<file_tree_node> findChild(const std::string& name) 
+  file_tree_node(const std::string& name, file_tree_node* parent = nullptr) : m_name(name), m_parent(parent) {};
+  ~file_tree_node() {for (auto child : m_children) delete child;}
+  file_tree_node* findChild(const std::string& name) 
   {
     for (auto child : m_children) if (child->m_name == name) return child;
     return nullptr;
   }
 
   std::string m_name;
-  std::shared_ptr<file_tree_node> m_parent;
-  std::vector<std::shared_ptr<file_tree_node>> m_children;
+  file_tree_node* m_parent;
+  std::vector<file_tree_node*> m_children;
   int m_size = 0;
-};
+} ftn;
 
-using ftn = file_tree_node;
-using ftnp = std::shared_ptr<ftn>;
-
-ftnp construct_tree(std::ifstream& file)
+ftn* construct_tree(std::ifstream& file)
 {
   std::string row;
-  ftnp node; 
+  ftn* node; 
   while (getline(file, row))
   {
     if (!row.find("$ cd"))
     {
-      if (row.at(5) == '/') node = std::make_shared<ftn>("/");
+      if (row.at(5) == '/') node = new ftn("/");
       else if (row.find("..") == 5) node = node->m_parent;
       else node = node->findChild(row.substr(5));
     }
-    else if (!row.find("dir")) 
-    {
-      node->m_children.push_back(std::make_shared<ftn>(row.substr(4)));
-      node->m_children.back()->m_parent = node;
-    }
-    else if (isdigit(row.front())) 
-    {
-      node->m_size += stoi(row.substr(0, row.find(' ')));
-    }
+    else if (!row.find("dir")) node->m_children.push_back(new ftn(row.substr(4), node));
+    else if (isdigit(row.front())) node->m_size += stoi(row.substr(0, row.find(' ')));
   }
   while (node->m_name != "/") node = node->m_parent;
   return node;
 }
 
-int calc_sizes(ftnp node)
+int calc_sizes(ftn* node)
 {
   if (!node->m_children.size()) return node->m_size;
   else for (auto child : node->m_children) node->m_size += calc_sizes(child);
   return node->m_size;
 }
 
-int part1(ftnp node)
+int part1(ftn* node)
 {
   int sum = 0;
   if (!node->m_children.size()) return node->m_size<100000?node->m_size:0;
@@ -60,7 +51,7 @@ int part1(ftnp node)
   return sum + (node->m_size<100000?node->m_size:0);
 }
 
-int part2(ftnp node, int minToDelete)
+int part2(ftn* node, int minToDelete)
 {
   if (!node->m_children.size()) return node->m_size;
   int min = node->m_size;
@@ -75,10 +66,11 @@ int part2(ftnp node, int minToDelete)
 std::pair<std::string, std::string> day7(std::ifstream file)
 {
   std::string row;
-  ftnp node = construct_tree(file);
+  ftn* node = construct_tree(file);
   int sum1 = 0;
   node->m_size = calc_sizes(node);
   int minToDelete = 30000000 - (70000000 - node->m_size);
-
-  return {std::to_string(part1(node)), std::to_string(part2(node, minToDelete))};
+  std::pair<std::string, std::string> result = {std::to_string(part1(node)), std::to_string(part2(node, minToDelete))};
+  delete node;
+  return result;
 }
