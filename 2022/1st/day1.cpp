5dbb215226b26254e55e92dc6527da3067d0ac24
Petteri Niemi <beteniemi@gmail.com>
2022-12-08 16:07:39 +0200

    Implement simple C++ solution for day 1 puzzle

--- 2022/1st/day1.cpp
+++ 2022/1st/day1.cpp
@@ -2,5 +2,21 @@
 
 std::pair<std::string, std::string> day1()
 {
-  return {"0", "0"};
+  std::ifstream file("inputs/input1.txt");
+  std::string row;
+  int max = 0;
+  int sum = 0;
+  while (getline(file, row))
+  {
+    if (row.size() > 0)
+    {
+      sum += std::stoi(row);
+    }
+    else
+    {
+      if (sum > max) max = sum;
+      sum = 0;
+    }
+  }
+  return {std::to_string(max), "0"};
 }
