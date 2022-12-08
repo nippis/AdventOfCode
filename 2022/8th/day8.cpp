a9d74de18ea3ea10514a61530da03ff3d01a8cc8
Petteri Niemi <beteniemi@gmail.com>
2022-12-08 11:57:57 +0200

    Shorten the code by making a function for boilerplate checks

--- 2022/8th/day8.cpp
+++ 2022/8th/day8.cpp
@@ -8,48 +8,48 @@ int calc_scenic_score( const std::vector<std::vector<std::pair<int, bool>>>& gri
   for (int row = coord.first-1; row >= 0; --row)
   {
     if (max >= grid.at(coord.first).at(coord.second).first) break;
-    if (grid.at(row).at(coord.second).first >= max)
-    {
-      max = grid.at(row).at(coord.second).first;
-    }
+    if (grid.at(row).at(coord.second).first >= max) max = grid.at(row).at(coord.second).first;
     scores.at(0)++;
   }
   max = -1;
   for (int row = coord.first+1; row < grid.size(); ++row)
   {
     if (max >= grid.at(coord.first).at(coord.second).first) break;
-    if (grid.at(row).at(coord.second).first >= max)
-    {
-      max = grid.at(row).at(coord.second).first;
-    }
+    if (grid.at(row).at(coord.second).first >= max) max = grid.at(row).at(coord.second).first;
     scores.at(1)++;
   }
   max = -1;
   for (int col = coord.second-1; col >= 0; --col)
   {
     if (max >= grid.at(coord.first).at(coord.second).first) break;
-    if (grid.at(coord.first).at(col).first >= max)
-    {
-      max = grid.at(coord.first).at(col).first;
-    }
+    if (grid.at(coord.first).at(col).first >= max) max = grid.at(coord.first).at(col).first;
     scores.at(2)++;
   }
   max = -1;
   for (int col = coord.second+1; col < grid.front().size(); ++col)
   {
     if (max >= grid.at(coord.first).at(coord.second).first) break;
-    if (grid.at(coord.first).at(col).first >= max)
-    {
-      max = grid.at(coord.first).at(col).first;
-    }
+    if (grid.at(coord.first).at(col).first >= max) max = grid.at(coord.first).at(col).first;
     scores.at(3)++;
   }
   int score = 1;
-  for (int x : scores)
-  {
-    score *= x;
-  }
+  for (int x : scores) score *= x;
   return score;
+}
+
+int is_visible(int row, int col, int& max,
+                std::vector<std::vector<std::pair<int, bool>>>& grid)
+{
+  if (grid.at(row).at(col).first > max)
+  {
+    max = grid.at(row).at(col).first;
+    if (!(grid.at(row).at(col).second))
+    {
+      grid.at(row).at(col).second = true;
+      return 1;
+    }
+  }
+  return 0;
 }
 
 std::pair<std::string, std::string> day8(std::ifstream file)
@@ -60,79 +60,34 @@ std::pair<std::string, std::string> day8(std::ifstream file)
   while (getline(file, row))
   {
     grid.push_back({});
-    for (char a : row)
-    {
-      grid.back().push_back({a-'0', false});
-    }
+    for (char a : row) grid.back().push_back({a-'0', false});
   }
   for (int row = 0; row < grid.size(); ++row)
   {
     int max = -1;
     for (int column = 0; column < grid.front().size(); ++column)
-    {
-      if (grid.at(row).at(column).first > max)
-      {
-        max = grid.at(row).at(column).first;
-        if (!(grid.at(row).at(column).second))
-        {
-          grid.at(row).at(column).second = true;
-          ++visible;
-        }
-      }
-    }
+      visible += is_visible(row, column, max, grid);
     max = -1;
     for (int column = grid.front().size()-1; column >= 0; --column)
-    {
-      if (grid.at(row).at(column).first > max)
-      {
-        max = grid.at(row).at(column).first;
-        if (!(grid.at(row).at(column).second))
-        {
-          grid.at(row).at(column).second = true;
-          ++visible;
-        }
-      }
-    }
+      visible += is_visible(row, column, max, grid);
   }
   for (int column = 0; column < grid.front().size(); ++column)
   {
     int max = -1;
     for (int row = 0; row < grid.size(); ++row)
-    {
-      if (grid.at(row).at(column).first > max)
-      {
-        max = grid.at(row).at(column).first;
-        if (!(grid.at(row).at(column).second))
-        {
-          grid.at(row).at(column).second = true;
-          ++visible;
-        }
-      }
-    }
+      visible += is_visible(row, column, max, grid);
     max = -1;
     for (int row = grid.size()-1; row >= 0; --row)
-    {
-      if (grid.at(row).at(column).first > max)
-      {
-        max = grid.at(row).at(column).first;
-        if (!(grid.at(row).at(column).second))
-        {
-          grid.at(row).at(column).second = true;
-          ++visible;
-        }
-      }
-    }
+      visible += is_visible(row, column, max, grid);
   }
 
   int max_score = 0;
-  for (int row = 1; row < grid.size()-1; ++row)
-  {
+  for (int row = 1; row < grid.size()-1; ++row) 
     for (int col = 1; col < grid.front().size()-1; ++col)
     {
       int score = calc_scenic_score(grid, {row, col});
       if (score > max_score) max_score = score;
     }
-  }
 
   return {std::to_string(visible), std::to_string(max_score)};
 }
