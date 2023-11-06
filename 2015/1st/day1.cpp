bbc045051874a1bdae365fdff4c43a913bad7add
Petteri Niemi <beteniemi@gmail.com>
2023-11-06 11:36:22 +0200

    Implement day1 of 2015 and fix template faults

--- 2015/1st/day1.cpp
+++ 2015/1st/day1.cpp
@@ -2,5 +2,14 @@
 
 std::pair<std::string, std::string> day1::solve(std::ifstream f)
 {
-  return {"",""};
+  char a;
+  int firstBasement;
+  int floor = 0;
+  int i = 0;
+  while (f.get(a) && ++i)
+  {
+    floor += a == '(' ? 1 : -1;
+    if ( firstBasement == 0 && floor < 0) firstBasement = i;
+  }
+  return { std::to_string(floor),std::to_string(firstBasement)};
 }
