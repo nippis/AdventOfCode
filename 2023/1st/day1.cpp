6ba9ff6de51765c5a0ee27a3a2ec5c44ef25b82a
Petteri Niemi <beteniemi@gmail.com>
2023-12-01 07:20:34 +0200

    The first part of the first day

--- 2023/1st/day1.cpp
+++ 2023/1st/day1.cpp
@@ -2,5 +2,22 @@
 
 std::pair<std::string, std::string> day1::solve(std::ifstream f)
 {
-  return {"",""};
+    std::string row = "";
+    int sum = 0;
+    while (getline(f, row)) 
+    {
+        char a = 0, b = 0;
+        for (char digit : row)
+        {
+          if (digit > 47 && digit < 58 && a == 0)
+            a = digit;
+          else  if (digit > 47 && digit < 58)
+            b = digit;
+        }
+        if (b == 0) b = a;
+        std::string sumstr = std::string(1, a)+std::string(1, b);
+        sum += stoi(sumstr);
+    }
+
+    return {std::to_string(sum), ""};
 }
