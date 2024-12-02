372b0d59ac440270205ee73dce8d338e7d75fb2a
Petteri Niemi <beteniemi@gmail.com>
2024-12-02 18:04:51 +0200

    Tidy up day 1 implementation

--- 2024/day01/day01.cpp
+++ 2024/day01/day01.cpp
@@ -14,20 +14,15 @@ std::pair<std::string, std::string> day01::solve(std::ifstream f)
 
   std::sort(idlist1.begin(), idlist1.end());
   std::sort(idlist2.begin(), idlist2.end());
-
   long long sum = 0;
   long long sum2 = 0;
-
+  
   for (int i = 0; i<idlist1.size(); ++i)
   {
     sum += abs(idlist1.at(i) - idlist2.at(i));
-
     for (int j = 0; j<idlist2.size(); ++j)
-    {
       if (idlist2.at(j) == idlist1.at(i))
-        sum2 += idlist1.at(i);
-    }
-
+        sum2 += (idlist2.at(j) == idlist1.at(i)) * idlist1.at(i);
   }
 
   return {std::to_string(sum), std::to_string(sum2)};
