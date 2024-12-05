dda196292b5c4a50f683e6092614888193067db6
Petteri Niemi <beteniemi@gmail.com>
2024-12-05 18:54:22 +0200

    Don't store updates to container

--- 2024/day05/day05.cpp
+++ 2024/day05/day05.cpp
@@ -3,20 +3,15 @@
 std::pair<std::string, std::string> day05::solve(std::ifstream f)
 {
   std::vector<std::pair<int, int>> rules;
-  std::vector<std::vector<int>> updates;
   std::string row;
   while (getline(f, row) && !row.empty())
     rules.push_back(std::make_pair(std::stoi(row.substr(0, 2)), std::stoi(row.substr(3, 2))));
+  int sum1 = 0, sum2 = 0;
   while (getline(f, row))
   {
-    updates.push_back({});
+    std::string update;
     for (int i = 0; i < row.size(); i += 3)
-      updates.back().push_back(std::stoi(row.substr(i, 2)));
-  }
-
-  int sum1 = 0, sum2 = 0;
-  for (auto update : updates)
-  {
+      update.push_back(std::stoi(row.substr(i, 2)));
     auto cmp = [=](int a, int b) 
     {
       for (auto r : rules) if (r.first == a && r.second == b || r.first == b && r.second == a) 
