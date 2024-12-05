e878ac64a338398803b00649e99d0e56fc6903cc
Petteri Niemi <beteniemi@gmail.com>
2024-12-05 19:23:26 +0200

    Rethink compare function

--- 2024/day05/day05.cpp
+++ 2024/day05/day05.cpp
@@ -5,21 +5,14 @@ std::pair<std::string, std::string> day05::solve(std::ifstream f)
   std::vector<std::pair<int, int>> rules;
   std::string row;
   while (getline(f, row) && !row.empty())
-    rules.push_back(std::make_pair(std::stoi(row.substr(0, 2)), std::stoi(row.substr(3, 2))));
+    rules.push_back({std::stoi(row.substr(0, 2)), std::stoi(row.substr(3, 2))});
   int sum1 = 0, sum2 = 0;
   while (getline(f, row))
   {
     std::string update;
-    for (int i = 0; i < row.size(); i += 3)
-      update.push_back(std::stoi(row.substr(i, 2)));
-    auto cmp = [=](int a, int b) 
-    {
-      for (auto r : rules) if (r.first == a && r.second == b || r.first == b && r.second == a) 
-        return r.first == a;
-      return false;
-    };
-    if (std::is_sorted(update.begin(), update.end(), cmp) && (sum1 += update.at(update.size()/2)))
-      continue;
+    for (int i = 0; i < row.size(); i += 3) update.push_back(std::stoi(row.substr(i, 2)));
+    auto cmp = [=](int a, int b) {return std::any_of(rules.begin(), rules.end(), [=](auto r) {return r.first == a && r.second == b;});};
+    if (std::is_sorted(update.begin(), update.end(), cmp) && (sum1 += update.at(update.size()/2))) continue;
     std::sort(update.begin(), update.end(), cmp);
     sum2 += update.at(update.size()/2);
   }
