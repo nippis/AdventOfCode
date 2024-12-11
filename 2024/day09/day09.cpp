6ebf2e41d569d6b4d5dbfe8b54176cdcc60249b5
Petteri Niemi <petteri.niemi@vertex.fi>
2024-12-11 10:06:47 +0200

    Day 9 part 2 on progress

--- 2024/day09/day09.cpp
+++ 2024/day09/day09.cpp
@@ -1,22 +1,41 @@
 #include "../solutions.hh"
 
+struct chunk
+{
+  int size, id;
+};
+
+std::vector<int>::iterator findSpace(std::vector<int>::iterator first, std::vector<int>::iterator last, int size)
+{
+  while (first != last)
+  {
+    for (int i = 0; i<size; ++i)
+    {
+      if (*(first+i) != -1)
+        break;
+      if (i == size-1)
+        return first;
+    }
+    ++first;
+  }
+}
+
 std::pair<std::string, std::string> day09::solve(std::ifstream f)
 {
   std::vector<int> disk;
+  std::vector<int> disk2(disk);
   char input;
   int id = 0;
   std::string row;
-  while (input = f.get())
+  while (f.get(input))
   {
-    if (input == std::char_traits<char>::eof())
-      break;
     disk.insert(disk.end(), input-48, id);
     id++;
-    input = f.get();
-    if (input == std::char_traits<char>::eof())
+    if (!f.get(input))
       break;
     disk.insert(disk.end(), input-48, -1);
   }
+
   int i = 0;
   while (i < disk.size())
   {
@@ -38,5 +57,12 @@ std::pair<std::string, std::string> day09::solve(std::ifstream f)
   long long sum = 0;
   for (int i = 0; i < disk.size(); ++i)
     sum += disk.at(i)*i;
+
+  int id = 10000;
+  for (int j = disk.size()-1; j >= 0; --j)
+  {
+    
+  }
+  
   return {std::to_string(sum),""};
 }
