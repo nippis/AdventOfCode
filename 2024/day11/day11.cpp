7d5c4419d8d4c32585e907f836d76dc78ae21856
Petteri Niemi <petteri.niemi@vertex.fi>
2024-12-11 16:00:39 +0200

    Optimize part 1 of day 11

--- 2024/day11/day11.cpp
+++ 2024/day11/day11.cpp
@@ -1,54 +1,54 @@
 #include "../solutions.hh"
+#include <thread>
+#include <future>
 
-int getDigitCount(const unsigned long long& x)
+std::vector<uint64_t> p = []()
 {
+  std::vector<uint64_t> p;
   for (int i = 0; i<21; ++i)
   {
-    if (x < std::pow(10, i))
-      return i;
+    p.push_back(std::pow(10, i));
   }
+  return p;
+}();
+
+int getDigitCount(const uint64_t& x)
+{
+  char i = 0;
+  while(x>=p.at(i)) ++i;
+  return i;
+}
+
+std::pair<uint64_t, uint64_t> splitNumber(const uint64_t& x)
+{
+  uint64_t div = p.at(getDigitCount(x)/2);
+  return std::make_pair(x/div, x%div);
+}
+
+std::vector<std::future<int>> threads;
+
+int blink(uint64_t stone, int n, int depth)
+{
+  if (n >= 25)
+    return 1;
+  std::vector<uint64_t> stones;
+  if (stone == 0)
+    return blink(1, n+1, depth);
+  else if (getDigitCount(stone) % 2 == 0)
+  {
+    auto numbers = splitNumber(stone);
+    return blink(numbers.first, n+1, depth) + blink(numbers.second, n+1, depth);
+  }
+  else
+    return blink(stone*2024, n+1, depth);
   return 0;
-}
-
-std::pair<unsigned long long, unsigned long long> splitNumber(const unsigned long long& x)
-{
-  unsigned long long first = x/(std::pow(10, getDigitCount(x)/2));
-  return std::make_pair(first, x-first*std::pow(10, getDigitCount(x)/2));
-}
-
-unsigned long long blink(std::list<unsigned long long>& stones)
-{
-  for (auto stone = stones.begin(); stone != stones.end(); ++stone)
-  {
-    if (*stone == 0)
-      *stone = 1;
-    else if (std::to_string(*stone).size() % 2 == 0)
-    {
-      auto numbers = splitNumber(*stone);
-      stones.insert(stone, numbers.first);
-      *stone = numbers.second;
-    }
-    else
-      *stone = (*stone)*2024;
-  }
-  return stones.size();
-}
-
-unsigned long long iterateBlinks(std::list<unsigned long long> stones, int n)
-{
-  unsigned long long sum = 0;
-  for (int i = 0; i<n; ++i)
-  {
-    sum += blink(stones);
-  }
-  return stones.size();
 }
 
 std::pair<std::string, std::string> day11::solve(std::ifstream f)
 {
   char input;
   std::string number;
-  std::list<unsigned long long> stones;
+  std::list<uint64_t> stones;
   while(f.get(input))
   {
     if (input != ' ')
@@ -61,7 +61,13 @@ std::pair<std::string, std::string> day11::solve(std::ifstream f)
   }
   stones.push_back(std::stoi(number));
 
-  unsigned long long sum = iterateBlinks(stones, 25);
+  long long sum = 0;
+  for (auto stone : stones)
+    sum += blink(stone, 0, 0);
+  for (auto& thread : threads)
+    sum += thread.get();
+
+  //std::vector<std::future<int>> threads; // std::async
 
   long long sum2 = 0;
 
