9bf585cff67e43e25f980b44d4206b05e9a79d0a
Petteri Niemi <beteniemi@gmail.com>
2025-12-01 12:36:12 +0200

    Create directory for 2025 implementation

---
+++ 2025/solutions.hh
@@ -0,0 +1,29 @@
+#ifndef SOLUTION_HH
+#define SOLUTION_HH
+
+#include <utility>
+#include <vector>
+#include <deque>
+#include <array>
+#include <unordered_set>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <set>
+
+std::string whitespace(int length);
+
+namespace day1  {std::pair<std::string, std::string> solve(std::ifstream file);}
+namespace day2  {std::pair<std::string, std::string> solve(std::ifstream file);}
+namespace day3  {std::pair<std::string, std::string> solve(std::ifstream file);}
+namespace day4  {std::pair<std::string, std::string> solve(std::ifstream file);}
+namespace day5  {std::pair<std::string, std::string> solve(std::ifstream file);}
+namespace day6  {std::pair<std::string, std::string> solve(std::ifstream file);}
+namespace day7  {std::pair<std::string, std::string> solve(std::ifstream file);}
+namespace day8  {std::pair<std::string, std::string> solve(std::ifstream file);}
+namespace day9  {std::pair<std::string, std::string> solve(std::ifstream file);}
+namespace day10 {std::pair<std::string, std::string> solve(std::ifstream file);}
+namespace day11 {std::pair<std::string, std::string> solve(std::ifstream file);}
+namespace day12 {std::pair<std::string, std::string> solve(std::ifstream file);}
+#endif
