9c61021d6c95559a3c3673fa6282c6eac93da32b
Petteri Niemi <beteniemi@gmail.com>
2025-12-01 20:45:56 +0200

    Solve day 1 puzzle

--- 2025/solutions/day01.cpp
+++ 2025/solutions/day01.cpp
@@ -2,5 +2,17 @@
 
 std::pair<std::string, std::string> day1::solve(std::ifstream f)
 {
-  return {"", ""};
+  std::string row = "";
+  unsigned int pw = 0;
+  unsigned int pw2 = 0;
+  int dial = 50;
+  int prev_dial = 0;
+  while (getline(f, row))
+  {
+    prev_dial = dial;
+    dial += std::stoi(row.replace(0, 1, row.at(0) == 'R' ? "-" : ""));
+    pw += dial % 100 == 0;
+    pw2 += abs(prev_dial/100 - dial/100) + (dial*prev_dial < 0);
+  }
+  return {std::to_string(pw), std::to_string(pw2)};
 }
