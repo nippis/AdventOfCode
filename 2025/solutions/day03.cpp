49ca9d6b1137d7f67eb96f8905fc22f71ae0ba6f
Petteri Niemi <beteniemi@gmail.com>
2025-12-03 10:41:34 +0200

    Solve day 3 puzzle part 2

--- 2025/solutions/day03.cpp
+++ 2025/solutions/day03.cpp
@@ -1,13 +1,22 @@
 #include "../solutions.hh"
+#include <cmath>
 
 std::pair<std::string, std::string> day3::solve(std::ifstream f)
 {
-  unsigned int result = 0;
+  unsigned int result1 = 0;
+  unsigned long long result2 = 0;
   std::string row;
   while (getline(f, row))
   {
     auto first = std::max_element(row.begin(), row.end()-1);
-    result += *first * 10 + *std::max_element(first+1, row.end()) - 480 - 48;
+    result1 += *first * 10 + *std::max_element(first+1, row.end()) - 480 - 48;
+
+    auto digitIterator = row.begin();
+    for (int i = 11; i >= 0; i--)
+    {
+      digitIterator = std::max_element(digitIterator, row.end()-i);
+      result2 += (*digitIterator++-48)*pow(10, i);
+    }
   }
-  return {std::to_string(result),""};
+  return {std::to_string(result1), std::to_string(result2)};
 }
