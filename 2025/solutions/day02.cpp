b9c42633f11add3ece7977e0e6a83b83edf2d1b6
Petteri Niemi <beteniemi@gmail.com>
2025-12-03 14:12:16 +0200

    Change day 2 implementation to string version

--- 2025/solutions/day02.cpp
+++ 2025/solutions/day02.cpp
@@ -3,35 +3,20 @@
 
 using int_type = unsigned long long;
 
-unsigned int digit_count(int_type a)
+int_type is_invalid(std::string a)
 {
-  return log10l(a)+1;
-}
-
-int_type subdigitset(int_type a, unsigned int pos, unsigned int count)
-{
-  if (digit_count(a)-pos < count)
-    return 0;
-  a = a / (int_type)pow(10, digit_count(a)-pos-count);
-  if (pos > 0)
-    a = a % (a / (int_type)pow(10, count) * (int_type)pow(10, count));
-  return a;
-}
-
-int_type is_invalid(int_type a)
-{
-  for (int i = 1; i <= digit_count(a)/2; i++)
+  for (int i = 1; i <= a.length()/2; i++)
   {
-    if (digit_count(a) % i != 0)
+    if (a.length() % i != 0)
       continue;
-    std::vector<int> sets;
-    for (int j = 0; j < digit_count(a) / i; j++)
+    std::vector<std::string> sets;
+    for (int j = 0; j < a.length() / i; j++)
     {
-      sets.push_back(subdigitset(a, j*i, i));
+      sets.push_back(a.substr(j*i, i));
     }
     if (std::all_of(sets.begin(), sets.end(), [=](auto i){return i == sets.at(0);}))
     {
-      return a;
+      return stoll(a);
     }
   }
   return 0;
@@ -44,7 +29,7 @@ std::pair<int_type, int_type> find_invalid_ids(const std::string& Begin, const s
   {
     std::string id = std::to_string(i);
     invalids.first += (id.substr(0, id.length()/2) == id.substr(id.length()/2)) ? stoll(id) : 0;
-    invalids.second += is_invalid(i) ? i : 0;
+    invalids.second += is_invalid(id) ? i : 0;
   }
   return invalids;
 }
