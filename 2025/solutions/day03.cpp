fdf4b0cb5982bbc7674420b317ffe361bab74a0e
Petteri Niemi <beteniemi@gmail.com>
2025-12-03 10:23:32 +0200

    Solve day 3 puzzle part 1

--- 2025/solutions/day03.cpp
+++ 2025/solutions/day03.cpp
@@ -2,5 +2,12 @@
 
 std::pair<std::string, std::string> day3::solve(std::ifstream f)
 {
-  return {"",""};
+  unsigned int result = 0;
+  std::string row;
+  while (getline(f, row))
+  {
+    auto first = std::max_element(row.begin(), row.end()-1);
+    result += *first * 10 + *std::max_element(first+1, row.end()) - 480 - 48;
+  }
+  return {std::to_string(result),""};
 }
