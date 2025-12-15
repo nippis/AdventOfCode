120ea79d1e0f51902ec79f29303bb880e7e10e4d
Petteri Niemi <beteniemi@gmail.com>
2025-12-15 13:36:13 +0200

    Use alias for solve function return type

--- 2025/solutions.hh
+++ 2025/solutions.hh
@@ -15,16 +15,18 @@
 std::string whitespace(std::size_t length);
 std::string whitespace(int length);
 
-namespace day1  {std::pair<std::string, std::string> solve(std::ifstream file);}
-namespace day2  {std::pair<std::string, std::string> solve(std::ifstream file);}
-namespace day3  {std::pair<std::string, std::string> solve(std::ifstream file);}
-namespace day4  {std::pair<std::string, std::string> solve(std::ifstream file);}
-namespace day5  {std::pair<std::string, std::string> solve(std::ifstream file);}
-namespace day6  {std::pair<std::string, std::string> solve(std::ifstream file);}
-namespace day7  {std::pair<std::string, std::string> solve(std::ifstream file);}
-namespace day8  {std::pair<std::string, std::string> solve(std::ifstream file);}
-namespace day9  {std::pair<std::string, std::string> solve(std::ifstream file);}
-namespace day10 {std::pair<std::string, std::string> solve(std::ifstream file);}
-namespace day11 {std::pair<std::string, std::string> solve(std::ifstream file);}
-namespace day12 {std::pair<std::string, std::string> solve(std::ifstream file);}
+using PuzzleResult = std::pair<std::string, std::string>;
+
+namespace day1  {PuzzleResult solve(std::ifstream file);}
+namespace day2  {PuzzleResult solve(std::ifstream file);}
+namespace day3  {PuzzleResult solve(std::ifstream file);}
+namespace day4  {PuzzleResult solve(std::ifstream file);}
+namespace day5  {PuzzleResult solve(std::ifstream file);}
+namespace day6  {PuzzleResult solve(std::ifstream file);}
+namespace day7  {PuzzleResult solve(std::ifstream file);}
+namespace day8  {PuzzleResult solve(std::ifstream file);}
+namespace day9  {PuzzleResult solve(std::ifstream file);}
+namespace day10 {PuzzleResult solve(std::ifstream file);}
+namespace day11 {PuzzleResult solve(std::ifstream file);}
+namespace day12 {PuzzleResult solve(std::ifstream file);}
 #endif
